a49e60ba642e1ecca6a7420e90b7ce70885fb238
MarcinCz <marcinczrw@gmail.com>
2013-04-25 22:55:39 +0200

    naprawa main

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-<<<<<<< HEAD
-    
-=======
-    //ads
->>>>>>> 5b6c64606d0541a9c8a6ec736b4ec0651f71e477
+
     return a.exec();
 }
